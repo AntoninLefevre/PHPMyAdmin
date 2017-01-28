1ed92057fd465792481898db177ab0d3a5a282e2
AntoninLefevre <antonin_08430@hotmail.fr>
2017-01-28 09:42:00 +0100

    Commandes list in index.php

--- index.php
+++ index.php
@@ -13,6 +13,24 @@ $html = "";
 
 foreach ($res as $dbname) {
     $html .= "<h1>" . $dbname['Database']. "</h1>";
+    $pdo = $bdd->prepare("use " . $dbname['Database']);
+    $pdo->execute();
+    $pdo = $bdd->prepare("SHOW TABLES");
+    $pdo->execute();
+    $tables = $pdo->fetchAll();
+
+    foreach ($tables as $table) {
+        $html .= "<h4>" . $table['Tables_in_' . $dbname['Database']] . "</h4>";
+
+        $pdo = $bdd->prepare("SHOW COLUMNS FROM " .$table['Tables_in_' . $dbname['Database']] );
+        $pdo->execute();
+
+        $columns = $pdo->fetchAll();
+
+        foreach ($columns as $column) {
+            $html .= "<h6>" . $column['Field'] . "</h6>";
+        }
+    }
 }
 
 
--- mypdo.include.php
+++ mypdo.include.php
@@ -1,5 +1,7 @@
 <?php
 	require_once 'classes/mypdo.class.php';
 
-	myPDO::setConfiguration('mysql:host=localhost;charset=utf8', 'root', '');
+    myPDO::setConfiguration('mysql:host=localhost;dbname=projet;charset=utf8', 'root', '');
+
+    //myPDO::setConfiguration('mysql:host=localhost;charset=utf8', 'root', '');
 ?>
