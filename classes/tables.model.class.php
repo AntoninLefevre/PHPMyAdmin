e1c1aa9f2b2fb26c677b3d0c127b65062359d560
lefeveAnthony <lefeve.anthony@gmail.com>
2017-01-28 10:38:16 +0100

    tables

--- classes/tables.model.class.php
+++ classes/tables.model.class.php
@@ -1,5 +1,24 @@
 <?php
 
 class Tables_Model{
+	private function __construct(){
 
+    }
+
+    public static function getTables($databases){
+        $bdd = MyPDO::getInstance();
+
+        $pdo = $bdd->prepare("USE $databases" );
+        $pdo->execute();
+		
+		$pdo = $bdd->prepare("SHOW TABLES");
+
+        $res = $pdo->fetchAll();
+
+        foreach ($res as $tables) {
+            $tables[] = $tables['Tables'];
+        }
+
+        return $Tables;
+    }
 }
--- classes/tables.view.class.php
+++ classes/tables.view.class.php
@@ -1,5 +1,16 @@
 <?php
 
 class Tables_View{
+	private function __construct(){
 
+    }
+
+    public static function listTables($tables){
+        $html = "";
+        foreach ($tables as $table) {
+            $html .= "<a href='?t=" . $table . "'>".$table."</a><br>";
+        }
+
+        return $html;
+    }
 }
