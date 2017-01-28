df8bac818294ada5af41337363a845d1db163ea6
lucielejeune <lucie.lejeune23@gmail.com>
2017-01-28 10:32:49 +0100

    class content

--- classes/content.model.class.php
+++ classes/content.model.class.php
@@ -1,5 +1,33 @@
 <?php
 
-class Content_View{
 
+class Content_Model{
+
+    private function __construct(){
+
+    }
+
+   
+
+    public static function getContent($table){
+        $bdd = MyPDO::getInstance();
+
+        $result = $bdd->prepare("select * from ".$table);
+        $result->execute();
+
+        $cols = $bdd->prepare("SHOW COLUMNS FROM " .$table);
+        $cols->execute();
+
+        // return
+        $columns = $cols->fetchAll();
+        $res = $result->fetchAll();
+
+        $tab[0]=$columns;
+        $tab[1]=$res;
+
+        return $tab;
+    }
 }
+
+
+?>
